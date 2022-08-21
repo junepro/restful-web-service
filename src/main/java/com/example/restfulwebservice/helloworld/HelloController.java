d7be5056f46e06305a826509380dc3a9489da139
junepro <57428866+junepro@users.noreply.github.com>
2022-08-21 16:46:26 +0900

    locale

--- src/main/java/com/example/restfulwebservice/helloworld/HelloController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloController.java
@@ -1,11 +1,19 @@
 package com.example.restfulwebservice.helloworld;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final MessageSource messageSource;
     //GET
     // /hello-world(endpoint)
     @GetMapping("/hello-world")
@@ -17,9 +25,16 @@ public class HelloController {
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("hello world");
     }
+
     @GetMapping("/hello-world-bean/path-value/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("hello world, %s,", name));
+    }
+
+    @GetMapping(path="/hello-world-international")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 
 
