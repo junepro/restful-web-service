7fd4a26d6e06dba6dbb9ca4429bbdec991497fce
junepro <57428866+junepro@users.noreply.github.com>
2022-09-04 20:07:53 +0900

    h2 init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into user
+values (1, sysdate(), 'User1', 'test1111', '701010-223132');
+
+insert into user
+values (2, sysdate(), 'User2', 'test222', '801010-223132');
+
+insert into user
+values (3, sysdate(), 'User3', 'test333', '901010-223132');
+
