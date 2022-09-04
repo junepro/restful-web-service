b64d5e087715fee080354ec9c61241da5b456743
junepro <57428866+junepro@users.noreply.github.com>
2022-09-04 19:35:45 +0900

    security, swagger etc

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,19 +15,24 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = {"password","ssn"})
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정볼르 위한 도메인 객체")
 public class User {
 
     private Integer id;
 
     @Size(min=2,message = "name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요")
     private Date joinDate;
 
    // @JsonIgnore
-    private String password;
+   @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요")
+   private String password;
     //@JsonIgnore
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요")
     private String ssn;
 
 
